c1d929c6039c9f58dab0d6e7de937a18a0b3dc2e
Michał Kurek <michal.kurek@mail.com>
2021-06-04 16:47:09 +0200

    init api

---
+++ Crunchyroll.Api/ICrunchyrollApi.cs
@@ -0,0 +1,18 @@
+﻿using Crunchyroll.Api.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Crunchyroll.Api
+{
+    /// <summary>
+    /// https://github.com/CloudMax94/crunchyroll-api/wiki/Api
+    /// </summary>
+    public interface ICrunchyrollApi: IDisposable
+    {
+        Task ListQueue(MediaType mediaType = MediaType.Default);
+
+        Task GetInfo();
+
+        //... etc
+    }
+}
---
+++ Crunchyroll.Api/Models/MediaType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Crunchyroll.Api.Models
+{
+    [Flags]
+    public enum MediaType
+    {
+        Anime = 0,
+        Drama = 1,
+
+        Default = Anime | Drama
+    }
+}
