69e50a86aa614781ab80e0232a9cedaa644110d8
Michał Kurek <michal.kurek@mail.com>
2021-06-05 14:18:03 +0200

    Add models

---
+++ Crunchyroll.Api/Models/QueueEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Crunchyroll.Api.Models
+{
+    public class QueueEntry
+    {
+        public Media LastWatchMedia { get; set; }
+
+        public Media MostLikelyMedia { get; set; }
+
+        public int Ordering { get; set; }
+
+        public int QueueEntryId { get; set; }
+
+        public int LastWatchMediaPlayhead { get; set; }
+
+        public int MostLikelyMediaPlayhead { get; set; }
+
+        public int Playhead { get; set; }
+
+        public Object Series { get; set; }
+    }
+}
