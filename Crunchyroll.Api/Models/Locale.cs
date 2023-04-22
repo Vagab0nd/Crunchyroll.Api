f32600d2159cf616e9b688cf656fd0a23f864696
Michał Kurek <michal.kurek@mail.com>
2023-04-22 17:01:15 +0200

    Add locale

---
+++ Crunchyroll.Api/Models/Locale.cs
@@ -0,0 +1,20 @@
+﻿namespace Crunchyroll.Api.Models
+{
+    public static class Locale
+    {
+        public const string JP = "ja-JP",
+            US = "en-US",
+            LA = "es-419",
+            LA2 = "es-LA",
+            ES = "es-ES",
+            FR = "fr-FR",
+            PT = "pt-PT",
+            BR = "pt-BR",
+            IT = "it-IT",
+            DE = "de-DE",
+            RU = "ru-RU",
+            AR = "ar-SA",
+            ME = "ar-ME",
+            CN = "zh-CN";
+    }
+}
