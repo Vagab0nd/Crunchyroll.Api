11dd4835fb7d32b7b0175e937cccc69c251808b0
Michał Kurek <Vagab0nd@users.noreply.github.com>
2023-04-22 17:55:56 +0200

    Dispose FormUrlEncodedContent

--- Crunchyroll.Api/Extensions/HttpClientExtensions.cs
+++ Crunchyroll.Api/Extensions/HttpClientExtensions.cs
@@ -9,12 +9,14 @@ namespace Crunchyroll.Api.Extensions
         public static async Task<HttpResponseMessage> PostFormUrlEncoded<TResult>(
             this HttpClient httpClient, string requestUri, IDictionary<string, string> postData)
         {
-            var content = new FormUrlEncodedContent(postData);
-            content.Headers.Clear();
-            content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            using (var content = new FormUrlEncodedContent(postData))
+            {
+                content.Headers.Clear();
+                content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
 
-            var response = await httpClient.PostAsync(requestUri, content);
-            return response;
+                var response = await httpClient.PostAsync(requestUri, content);
+                return response;
+            }
         }
     }
 }
