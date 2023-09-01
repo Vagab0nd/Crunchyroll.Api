ea4f708d13811d856999f5d65ea5e6fb996240fd
Michał Kurek <Vagab0nd@users.noreply.github.com>
2023-09-01 20:56:18 +0200

    Use Flurl to manage API calls

---
+++ Crunchyroll.Api/Infrastructure/Extensions/UrlExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Flurl;
+using Flurl.Http;
+
+namespace Crunchyroll.Api.Infrastructure.Extensions
+{
+    internal static class UrlExtensions
+    {
+        private const string authorizationHeaderValue = "Basic aHJobzlxM2F3dnNrMjJ1LXRzNWE6cHROOURteXRBU2Z6QjZvbXVsSzh6cUxzYTczVE1TY1k=";
+
+        public static IFlurlRequest WithCrunchyrollBasicAuth(this Url url)
+        {
+            return url.WithHeader("Authorization", authorizationHeaderValue);
+        }
+    }
+}
